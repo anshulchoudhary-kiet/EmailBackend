fe69413128f3040c756a111edd33a2fb03fba46e
anshul-choudhary-kiet <choudharyanshul212@gmail.com>
2025-02-09 12:03:25 +0530

    udpdate

--- src/main/java/com/email/writer/EmailWriterSbApplication.java
+++ src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -3,7 +3,7 @@ package com.email.writer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.email.writer")
+@SpringBootApplication
 public class EmailWriterSbApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +11,4 @@ public class EmailWriterSbApplication {
 	}
 
 }
+//(scanBasePackages = "com.email.writer")
